7163a50d253528bd29bb9ecac15fb9733496dc4c
Abideen Olafimihan <olafimihana@gmail.com>
2022-05-24 06:18:18 +0100

    updating files

--- js/product.js
+++ js/product.js
@@ -1,3 +1,4 @@
+// CHANGING THE BACKGROUND IMAGES
 // select the images
 const images = document.querySelectorAll('#productImage img',);
 // select the image sliders
@@ -16,5 +17,24 @@ images.forEach( (item, i) => {
         imageSlider.style.backgroundImage = `url(${item.src})`
         // set currentImage to the index of the clicked image to remove active class;
         currentImage = i;
+    })
+})
+
+
+// CHANGING THE SIZES
+// select all the size button label
+const sizeButton = document.querySelectorAll('#sizeButton label');
+// set inital counter
+let currentSize = 0;
+
+// loop through the size buttons
+sizeButton.forEach( (item, i) => {
+    item.addEventListener('click', () => {
+        // remove the active class from the first size button
+        sizeButton[currentSize].classList.remove('check');
+        // add check class to the size buttons when clicked
+        item.classList.add('check');
+        // set currentSize to the index of the clicked 
+        currentSize = i;
     })
 })
