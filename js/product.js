efc510ac5693eeae2644f4ad7f893db634f56256
Abideen Olafimihan <olafimihana@gmail.com>
2022-05-24 05:35:44 +0100

    updating files

--- js/product.js
+++ js/product.js
@@ -1,1 +1,20 @@
-// select
+// select the images
+const images = document.querySelectorAll('#productImage img',);
+// select the image sliders
+const imageSlider = document.getElementById('imageSlider');
+// set initial counter
+let currentImage = 0;
+
+// loop through all images
+images.forEach( (item, i) => {
+    item.addEventListener('click', () => {
+        // remove the active class in the first image(s)
+        images[currentImage].classList.remove('active');
+        // add active class to the images
+        item.classList.add('active');
+        // change the background-image of the image-slider
+        imageSlider.style.backgroundImage = `url(${item.src})`
+        // set currentImage to the index of the clicked image to remove active class;
+        currentImage = i;
+    })
+})
